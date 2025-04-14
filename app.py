613de23f53303bf907484705b4072a99e614d025
Pranav1102004 <prs11102004@gmail.com>
2025-04-14 22:05:19 +0530

    fixed bind to render

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 from flask import Flask, request,send_file
 import soundfile as sf
 import io
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,6 @@ def genrate():
     return send_file(buffer,mimetype='audio/wav',as_attachment=True,download_name='binaural.wav')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
